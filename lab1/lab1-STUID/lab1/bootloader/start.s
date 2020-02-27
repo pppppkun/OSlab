9c4ebb45105d9d6ca85893a64877b1a9e0115b0a
LiChun <181250068@smail.nju.edu.cn>
2020-02-27 12:57:42 +0800

    fix

--- lab1/lab1-STUID/lab1/bootloader/start.s
+++ lab1/lab1-STUID/lab1/bootloader/start.s
@@ -25,11 +25,12 @@ start:
 start32:
 	movw $0x10,%ax
 	movw %ax,%ds
+	movw %ax,%es
 	movw %ax,%ss
-	movw %ax,%fs
 	movw $0x18,%ax
+	movw %ax,%fs
 	movw %ax,%gs
-	movw $0x8c00,%eax
+	movw $0x7e00,%eax
 	movw %eax,%esp
 	jmp bootMain
 
