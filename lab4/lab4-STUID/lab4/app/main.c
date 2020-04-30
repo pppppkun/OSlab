4880f4518ec69baa2b8e7d3f489766f462535135
pkun <181250068@smail.nju.edu.cn>
2020-05-01 00:08:14 +0800

    sem_init sem_wait sem_post sem_destroy are done!

--- lab4/lab4-STUID/lab4/app/main.c
+++ lab4/lab4-STUID/lab4/app/main.c
@@ -1,8 +1,9 @@
 #include "lib.h"
 #include "types.h"
 
-int uEntry(void) {
-/*	int dec = 0;
+int uEntry(void)
+{
+	/*	int dec = 0;
 	int hex = 0;
 	char str[6];
 	char cha = 0;
@@ -15,7 +16,7 @@ int uEntry(void) {
 			break;
 	} */
 
-/*	int data = 2020;
+	/*	int data = 2020;
 	int data1 = 1000;
 	int i = 4;
 	int ret = fork();
@@ -37,25 +38,68 @@ int uEntry(void) {
 		} 
 		exit();
 	}
+	
+	printf("scanf seccess!\n");
 	return 0; */
 
+	int i = 4;
+	int ret = 0;
+	int value = 2;
+	sem_t sem;
+	printf("Father Process: Semaphore Initializing.\n");
+	ret = sem_init(&sem, value);
+	if (ret == -1)
+	{
+		printf("Father Process: Semaphore Initializing Failed.\n");
+		exit();
+	}
+	ret = fork();
+	if (ret == 0)
+	{
+		while (i != 0)
+		{
+			i--;
+			printf("Child Process: Semaphore Waiting.\n");
+			sem_wait(&sem);
+			printf("Child Process: In Critical Area.\n");
+		}
+		printf("Child Process: Semaphore Destroying.\n");
+		sem_destroy(&sem);
+		exit();
+	}
+	else if (ret != -1)
+	{
+		while (i != 0)
+		{
+			i--;
+			printf("Father Process: Sleeping.\n");
+			sleep(128);
+			printf("Father Process: Semaphore Posting.\n");
+			sem_post(&sem);
+		}
+		printf("Father Process: Semaphore Destroying.\n");
+		sem_destroy(&sem);
+		exit();
+	}
 
-	printf("scanf seccess!\n");
+	return 0;
+
 	char ch;
 	printf("Input: 1 for bounded_buffer\n       2 for philosopher\n       3 for reader_writer\n");
 	scanf("%c", &ch);
-	switch (ch) {
-		case '1':
-			exec("/usr/bounded_buffer", 0);
-			break;
-		case '2':
-			exec("/usr/philosopher", 0);
-			break;
-		case '3':
-			exec("/usr/reader_writer", 0);
-			break;
-		default:
-			break;
+	switch (ch)
+	{
+	case '1':
+		exec("/usr/bounded_buffer", 0);
+		break;
+	case '2':
+		exec("/usr/philosopher", 0);
+		break;
+	case '3':
+		exec("/usr/reader_writer", 0);
+		break;
+	default:
+		break;
 	}
 	exit();
 	return 0;
--- lab4/lab4-STUID/lab4/bounded_buffer/main.c
+++ lab4/lab4-STUID/lab4/bounded_buffer/main.c
@@ -1,8 +1,59 @@
 #include "lib.h"
 #include "types.h"
+#define N 4
+int buffer[N];
+int setPoint = 0;
+int getPoint = 0;
+void Deposit(int i, sem_t *eb, sem_t *fb, sem_t *mutex)
+{
+	sem_wait(eb);
+	sem_wait(mutex);
+	buffer[setPoint] = 2;
+	setPoint = (setPoint + 1) % N;
+	sem_post(mutex);
+	sem_post(fb);
+}
+
+void Remove(int i, sem_t *eb, sem_t *fb, sem_t *mutex)
+{
+	sem_wait(fb);
+	sem_wait(mutex);
+
+}
 
 int main(void) {
 	// TODO in lab4
+
+	sem_t mutex;
+	sem_t fullBuffers;
+	sem_t emptyBuffers;
+	int ret = 0;
+	ret = sem_init(&mutex, 1);
+	if(ret==-1)
+	{
+		printf("init mutex error\n");
+		return 0;
+	}
+	ret = sem_init(&fullBuffers, 0);
+	if(ret==-1)
+	{
+		printf("init fullBuffers error\n");
+		return 0;
+	}
+	ret = sem_init(&emptyBuffers, N);
+
+	ret = fork();
+	if(ret==0)
+	{
+		
+	}
+	else
+	{
+		
+	}
+	
+
+
 	printf("bounded_buffer\n");
 	exit();
 	return 0;
